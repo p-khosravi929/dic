c7e5232264077f62707d43d0595ab300aa9aad11
p-khosravi929 <pylabs.ai@gmail.com>
2025-10-23 16:18:13 +0330

    Create dic package structure

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+DIC - Drought Indices Calculation
+A Python package for calculating various drought indices.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Parviz Khosravi"
+__email__ = "parviz.khosravi80@gmail.com"
+
+from dic.indices.czi import ChinaZIndex
+
+__all__ = ['ChinaZIndex']
