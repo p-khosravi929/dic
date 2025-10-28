e2bdf7fe8f23d821df5e65cdfa89834e3b7758fd
p-khosravi929 <pylabs.ai@gmail.com>
2025-10-28 14:24:14 +0330

    Add ModifiedChinaZIndex to indices exports

--- dic/indices/__init__.py
+++ dic/indices/__init__.py
@@ -1,4 +1,5 @@
 from .czi import ChinaZIndex
 from .ci import CompositeIndex
+from .mczi import ModifiedChinaZIndex
 
-__all__ = ['ChinaZIndex', 'CompositeIndex']
+__all__ = ['ChinaZIndex', 'CompositeIndex', 'ModifiedChinaZIndex']
